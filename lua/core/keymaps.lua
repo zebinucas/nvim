0e48c2b65654e385efad8bcc70b18e697c62fab7
zebinucas <zhangzebin0011@gmail.com>
2023-12-15 16:04:05 +0800

    添加基础配置和移动映射

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,7 +2,11 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+-- keymap语法
+-- keymap.set("mode", "new option", "old option")
+
 -- ---------- 插入模式 ---------- ---
+-- 回到normal模式
 keymap.set("i", "jj", "<ESC>")
 
 -- ---------- 视觉模式 ---------- ---
@@ -11,9 +15,17 @@ keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- ---------- 正常模式 ---------- ---
+-- 保存并退出
+keymap.set("n", "<leader>wq", ":wq<ENTER>")
 -- 窗口
 keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
 keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+keymap.set("n", "<C-l>", "<C-w>l") -- 水平切换窗口
+keymap.set("n", "<C-h>", "<C-w>h") -- 水平切换窗口
+keymap.set("n", "<C-j>", "<C-w>j") -- 垂直切换窗口
+keymap.set("n", "<C-k>", "<C-w>k") -- 垂直切换窗口
+-- 打开命令行 windows 配置到默认打开pwsh
+keymap.set("n", "<leader>t", ":ter pwsh<ENTER>")
 
--- 取消高亮
+-- 搜索后取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -20,8 +20,17 @@ opt.cursorline = true
 opt.mouse:append("a")
 
 -- 系统剪贴板
+-- nvim默认是复制到缓冲区不会复制到系统的剪切板，这里设置为复制到剪切板
 opt.clipboard:append("unnamedplus")
-
+-- 在 copy 后高亮
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	pattern = { "*" },
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 300,
+		})
+	end,
+})
 -- 默认新窗口右和下
 opt.splitright = true
 opt.splitbelow = true
